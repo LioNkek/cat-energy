12940a925150907537b2c2354bde2a4a3b25f268
Lev <lion.gg.k@gmail.com>
2026-03-10 15:34:32 +0700

    feat: добавление функционала для переключения навигационного меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-nav');
+const mainNavToggle = document.querySelector('.main-nav--toggle');
+
+mainNav.classList.remove('main-nav--nojs');
+
+mainNavToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+  }
+});
