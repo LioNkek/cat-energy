f2770b1829a12b4c43cfbfc5b8c381c3f1fbcc86
Lev <lion.gg.k@gmail.com>
2026-04-10 16:50:43 +0700

    слайдер работа 2

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -10,3 +10,13 @@ mainHeaderToggle.addEventListener('click', () => {
   mainNav.classList.toggle('main-nav--closed');
 });
 
+//slider
+const range = document.querySelector('.slider__range');
+const button = document.querySelector('.slider__button');
+const overlay = document.querySelector('.slider__image-overlay');
+
+range.addEventListener('input', (e) => {
+  const value = e.target.value;
+  button.style.left = `${value}%`;
+  overlay.style.clipPath = `inset(0 0 0 ${value}%)`;
+});
