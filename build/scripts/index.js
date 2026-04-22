c91a89645db750866e845412425bac0277146227
Lev <lion.gg.k@gmail.com>
2026-04-22 09:45:12 +0700

    fix

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -1,1 +1,18 @@
-var n=document.querySelector(".main-nav"),e=document.querySelector(".main-header__toggle");n.classList.remove("main-nav--nojs");e.classList.remove("main-header__toggle--nojs");e.addEventListener("click",()=>{e.classList.toggle("main-header__toggle--opened"),n.classList.toggle("main-nav--closed")});var a=document.querySelector(".slider__range"),s=document.querySelector(".slider__button"),l=document.querySelector(".slider__image-overlay");a.addEventListener("input",o=>{let t=o.target.value;s.style.left=`${t}%`,l.style.clipPath=`inset(0 0 0 ${t}%)`});
+// source/scripts/index.js
+var mainNav = document.querySelector(".main-nav");
+var mainHeaderToggle = document.querySelector(".main-header__toggle");
+mainNav.classList.remove("main-nav--nojs");
+mainHeaderToggle.classList.remove("main-header__toggle--nojs");
+mainHeaderToggle.addEventListener("click", () => {
+  mainHeaderToggle.classList.toggle("main-header__toggle--opened");
+  mainNav.classList.toggle("main-nav--closed");
+});
+var range = document.querySelector(".slider__range");
+var button = document.querySelector(".slider__button");
+var overlay = document.querySelector(".slider__image-overlay");
+range.addEventListener("input", (e) => {
+  const value = e.target.value;
+  button.style.left = `${value}%`;
+  overlay.style.clipPath = `inset(0 0 0 ${value}%)`;
+});
+//# sourceMappingURL=index.js.map
